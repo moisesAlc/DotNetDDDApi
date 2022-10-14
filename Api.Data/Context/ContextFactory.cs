f3abe410eef99cc2412c7ae0ce6f8ff890a14b87
Moisés Madeira <msalcmd@gmail.com>
2022-10-14 09:08:57 -0300

    Added ContextFactory class

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            // Usado para criar as migrações
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=admin;Pwd=@1234,abcd";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
