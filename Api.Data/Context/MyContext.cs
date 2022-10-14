81a734f6ca5235cfab78120452d3be012df87411
Moisés Madeira <msalcmd@gmail.com>
2022-10-14 09:08:36 -0300

    added MyContext class

---
+++ Api.Data/Context/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Context
+{
+    public class MyContext:DbContext
+    {
+        public DbSet<UserEntity> Users{ get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base (options)  {}
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating (modelBuilder);
+        }
+    }
+}
