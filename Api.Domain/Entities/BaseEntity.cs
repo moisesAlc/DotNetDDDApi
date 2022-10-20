c6f0159eb99543b3966d3e28068bd11b44a74d3b
Moisés Madeira <msalcmd@gmail.com>
2022-10-20 14:48:28 -0300

    BaseEntity & UserEntity

--- Api.Domain/Entities/BaseEntity.cs
+++ Api.Domain/Entities/BaseEntity.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
-namespace Domain.Entities
-{
-    public abstract class BaseEntity
-    {
-		[Key]
+namespace Api.Domain.Entities {
+    public abstract class BaseEntity {
+        [Key]
         public Guid Id { get; set; }
+        private DateTime? _createAt;
+        public DateTime? CreateAt {
+            get { return _createAt; }
+            set { _createAt = (value == null ? DateTime.UtcNow : value); }
+        }
 
-		private DateTime? _createdAt;
+        public DateTime? UpdateAt { get; set; }
 
-		public DateTime? CreatedAt
-		{
-			get { return _createdAt; }
-			set { _createdAt = ( value == null ? DateTime.UtcNow : value ); }
-		}
-
-		public DateTime? UpdatedAt { get; set; }
-
-	}
+    }
 }
--- Api.Domain/Entities/UserEntity.cs
+++ Api.Domain/Entities/UserEntity.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Domain.Entities
-{
-    public class UserEntity:BaseEntity
-    {
+namespace Api.Domain.Entities {
+    public class UserEntity : BaseEntity {
         public string Name { get; set; }
         public string Email { get; set; }
-
     }
 }
